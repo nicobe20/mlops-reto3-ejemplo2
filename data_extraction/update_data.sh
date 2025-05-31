e074a6d642e13c6ad12cba2daf5c5e71661cd9b2
edwinm67 <emontoya@eafit.edu.co>
2025-05-31 06:53:13 -0500

    restart

--- data_extraction/update_data.sh
+++ data_extraction/update_data.sh
@@ -1,6 +1,6 @@
 git config --global user.name 'Edwin Montoya'
 git config --global user.email 'emontoya@eafit.edu.co'
-# DEBE CONFIGURAR UNA VARIABLE DE AMBIENTE GH_TOKEN EN Github-Setttings-> Develope Settings -> Personal access token - > Tokens
+# DEBE CONFIGURAR UNA VARIABLE DE AMBIENTE GH_TOKEN EN Github-Setttings-> Developer Settings -> Personal access token - > Tokens
 # LUEGO DENTRO DEL REPOSITORIO: Settings -> Secrets and variables -> Actions -> Secrets -> Environment secrets
 #git remote set-url origin https://x-access-token:${{ secrets.GH_TOKEN }}@github.com/edwinm67/mlops-ejemplo2.git
 git add .
